591b23ab616d239e67b8d564a4d9178f35e04904
Fernando Denis <supergogleyoutu@gmail.com>
2023-11-18 20:53:28 -0600

    Agregar archivos de proyecto.

---
+++ Base_Datos/Server/Controllers/LigasController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Base_Datos.Server.Data;
+using Base_Datos.Shared.Modelos;
+
+namespace Base_Datos.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LigasController : ControllerBase
+    {
+        private readonly DataFutbolDBContext _context;
+
+        public LigasController(DataFutbolDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Ligas
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Liga>>> GetLigas()
+        {
+          if (_context.Ligas == null)
+          {
+              return NotFound();
+          }
+            return await _context.Ligas.ToListAsync();
+        }
+
+        // GET: api/Ligas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Liga>> GetLiga(int id)
+        {
+          if (_context.Ligas == null)
+          {
+              return NotFound();
+          }
+            var liga = await _context.Ligas.FindAsync(id);
+
+            if (liga == null)
+            {
+                return NotFound();
+            }
+
+            return liga;
+        }
+
+        // PUT: api/Ligas/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutLiga(int id, Liga liga)
+        {
+            if (id != liga.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(liga).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LigaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Ligas
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Liga>> PostLiga(Liga liga)
+        {
+          if (_context.Ligas == null)
+          {
+              return Problem("Entity set 'DataFutbolDBContext.Ligas'  is null.");
+          }
+            _context.Ligas.Add(liga);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetLiga", new { id = liga.Id }, liga);
+        }
+
+        // DELETE: api/Ligas/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteLiga(int id)
+        {
+            if (_context.Ligas == null)
+            {
+                return NotFound();
+            }
+            var liga = await _context.Ligas.FindAsync(id);
+            if (liga == null)
+            {
+                return NotFound();
+            }
+
+            _context.Ligas.Remove(liga);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool LigaExists(int id)
+        {
+            return (_context.Ligas?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Base_Datos/Shared/Modelos/Liga.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base_Datos.Shared.Modelos
+{
+    public class Liga
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Debe escribir el nombre de la liga")]
+        [StringLength(100)]
+        public string? Nombre_liga { get; set; }
+        [Required(ErrorMessage = "Debe escribir el nombre de la confederacion")]
+        [StringLength(50)]
+        public string? Confederacion { get; set; }
+        [Required(ErrorMessage = "Debe escribir el nombre del pais")]
+        [StringLength(50)]
+        public string? Pais { get; set; }
+        [Required(ErrorMessage = "Debe escribir el URL de la imagen")]
+        [StringLength(250)]
+        public string? Logo { get; set; }
+    }
+}
